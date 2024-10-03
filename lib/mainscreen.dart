fbcfe51e9fc495c65de2e0fcbdc8174b85068a24
vishalsanthosh <vishalsanthosh23@gmail.com>
2024-10-03 22:21:27 +0530

    finished

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -16,147 +16,145 @@ class Mainscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 11, 105, 212),
       body: Column(
         children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Container(
-                  height: 40,
-                  width: 100,
-                  color: Colors.blue,
-                  child: Center(child: Text("FLIPKART",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
-                ),
-              ),
-              Spacer(),
-              Image.asset("assets/images/logo.jpeg",height: 50,
-              width: 50,),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Container(
-                   height: 40,
-                    width: 100,
-                    color: Colors.blue,
-                    child: Center(child:Icon(Icons.local_grocery_store),
-                    
-                    
-                     
-                     ),
-                ),
-              ),
-              
-              
-          ],
-          ),
-          SizedBox(height: 5,),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 
-                decoration: BoxDecoration(border: Border.all(color: Colors.black,),color: Colors.white,borderRadius: BorderRadius.circular(26,)),
-                height: 50,
-                width: double.infinity,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 45,
+                      width: 250,
+                      
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.white,),
+                     child: Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: Row(
+                        children: [
+                          Icon(Icons.search,color: Colors.grey),
+                           Text("Flipkart",style: TextStyle(color: Colors.blue),),
+                           Spacer(),
+                           Image.asset("assets/images/logo.jpeg",height: 100,width: 60,)
+                        ],
+                       ),
+                     ),
+                      ),
+                  ],
+                ),
+                Spacer(),
+                Icon(Icons.favorite,color: Colors.red,size: 30,),
+                Spacer(),
+                Icon(Icons.shopping_cart,color: const Color.fromARGB(255, 1, 12, 41),size: 30,),
+            
+              ]
+                  ),
+          ),
+           
+           SizedBox(height: 10,),
+          CarouselSlider( options: CarouselOptions(
+            height: 200,
+            aspectRatio: 9/16,
+            autoPlay: true,
+            enlargeCenterPage: true,
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            viewportFraction: 0.8,
+          ),
+      
+          items: imageUrl.map((imageUrl){
+       return Container(
+          height: 200,
+          width: double.infinity,
+          margin: EdgeInsets.symmetric(horizontal: 0.5),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+          ),
+          child: Image.asset(imageUrl,
+          fit: BoxFit.cover,),
+        );
+      
+          }).toList(),
+          ),
+      
+        SizedBox(height: 10),
+      
+                
+         
+       
+      SizedBox(height: 15),
+      Expanded(
+        child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 9/9,
+          crossAxisCount: 2), 
+          itemCount: 4,
+          itemBuilder: (context,index){
+          return Container(
+          height: 500,
+          width: 500,
+          decoration: BoxDecoration(border: Border.all(color: const Color.fromARGB(255, 204, 203, 203)),color: Colors.white),
+           child:Column(
+             mainAxisAlignment: MainAxisAlignment.start,
+             crossAxisAlignment: CrossAxisAlignment.start,
+             children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> DescriptionScreenExp(imagePath: Database.phoneList[index]["image"],
+                  name:Database.phoneList[index]["name"],
+                  price:Database.phoneList[index]["price"],
+                  description: Database.phoneList[index]["des"],
+                  )));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage(Database.phoneList[index]["image"])),
+                    
+                 ),
+                 height: 115,
+                 width: double.infinity,
+                ),
+              ),
+              SizedBox(height: 10,),
+               Center(child: Text(Database.phoneList[index]["name"],style: TextStyle(fontSize: 14,color: Colors.blue[600],fontWeight: FontWeight.w800),)),
+
+               
+            Container(
+              height: 20,
+              width: 130,
+              child: Center(child: Text("Flipkart Assured",style: TextStyle(fontSize: 13),)),
+              color: const Color.fromARGB(255, 4, 145, 226),
+             ),
+
+
+               Row(
                     children: [
-                      Icon(Icons.search,size: 25,color: Colors.blue,),
-                      Text("Search Phones...............",style: TextStyle(color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),),
-                      Spacer(),
-                      Icon(Icons.mic,color: Colors.blue,size: 25,),
-                      Icon(Icons.camera,size: 25,color: Colors.red,)
+                      Icon(Icons.currency_rupee,size: 14,),
+                      Center(child: Text(Database.phoneList[index]["price"],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14),)),
+                      
+                    
+                    
                     ],
                   ),
-                )
-              ),
-            ),
-          ),
-          SizedBox(height: 1,),
-          Column(
-            children: [
-              CarouselSlider( options: CarouselOptions(
-                height: 100,
-                autoPlay: true,
-                enlargeCenterPage: true,
-                aspectRatio: 16/9,
-                autoPlayCurve: Curves.easeInOutCubicEmphasized,enableInfiniteScroll: true,
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                viewportFraction: 0.8
-              ),
-              items: 
-                imageUrl.map((imageUrl){
-                  return Container(
-                    height: 400,
-                    width: double.infinity,
-                    margin: EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Image.asset(imageUrl,fit: BoxFit.cover,),
-                  );
-                }).toList()
-                
-              
-              )
-              
-            ],
-          ),
-          
-          Expanded(
-            child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-              childAspectRatio: 6/9,
-              
-              crossAxisCount: 2),
-              itemCount: 4,
-               itemBuilder: (BuildContext context,int index){
-                return Container(
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color: Colors.white70),
                 
 
-                height: 500,
-                width: 500,
-                
-                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: (){
-                        Navigator.push(context,MaterialPageRoute(builder: (context)=>DescriptionScreenExp(
-                          imagePath: Database.phoneList[index]["image"], name: Database.phoneList[index]["name"],
-                           price: Database.phoneList[index]["price"],
-                            description: Database.phoneList[index]["des"])));
-                      },
-                    ),
-                    Container(
-                      decoration: BoxDecoration(image: DecorationImage(image: AssetImage(Database.phoneList[index]["image"],))),height: 118,
-                      width: double.infinity,
-                      child:Expanded(
-                        child: Column(
-                           children: [
-                           
-                           Row(
-                            children: [
-                              Text(Database.phoneList[index]["name"]),
-                              Spacer(),
-                              Icon(Icons.currency_rupee),
-                              Text(Database.phoneList[index]["price"])
-                            ],
-                           
-                        
-                                            )]),
-                      )
-                 )]
-                 )
-                );
-               }),
-          )
 
-        ],
+
+             ],
+           ),
+                         );
+      }
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
+      ]),
+       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.black,
         items:[
@@ -172,7 +170,4 @@ class Mainscreen extends StatelessWidget {
           ),
           
         ]),
-    );
-    
-  }
-}
+      );}}
